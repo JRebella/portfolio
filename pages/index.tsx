2b21f6eaf2bf40484a561edbd1566138e4285fdc
JRebella <jrebella96@gmail.com>
2022-03-04 20:30:57 -0300

    feat: added FA icons, PDF download, basic landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Button from "../components/Button";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload, faFilePdf } from "@fortawesome/free-solid-svg-icons";
+
 import styles from "./home.module.scss";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -16,7 +22,39 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to my Portfolio</h1>
+        <section className={styles.section}>
+          <div className={styles["named-avatar"]}>
+            <div className={styles.avatar}>
+              <Image
+                src="/profile.png"
+                alt=""
+                width={120}
+                height={120}
+                className={styles.avatar}
+              />
+            </div>
+            <div>
+              <h1 className={styles.title}>Juan Rebella</h1>
+              <h2 className={styles.subtitle}>Software Engineer</h2>
+            </div>
+          </div>
+
+          <div className={styles.buttons}>
+            <div className={styles["button-container"]}>
+              <span>Jump right in</span>
+              <Button>My Portfolio</Button>
+            </div>
+
+            <div className={styles["button-container"]}>
+              <span>On a hurry?</span>
+              <Link href="/Juan Rebella Jan 2022 ENG.pdf" passHref>
+                <Button>
+                  Download CV (.pdf) <FontAwesomeIcon icon={faDownload} />
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
