173c46a60ccf5ec316bb43d5ab2c85da540d44b5
JRebella <jrebella96@gmail.com>
2022-03-08 21:49:29 -0300

    fix: WIP about section

--- pageComponents/about/aboutSection.tsx
+++ pageComponents/about/aboutSection.tsx
@@ -10,17 +10,17 @@ const AboutSection: FunctionComponent<ComponentProps<"section">> = () => {
       <div>
         <p>
           Hey there, I'm <span className="text-accent">Juan</span>. I'm a{" "}
-          <span className="text-accent">software engineer from Uruguay</span>{" "}
-          and I specialize myself on frontend development with{" "}
+          <span className="text-accent">software engineer from Uruguay</span> and I
+          specialize myself on frontend development with{" "}
           <span className="text-accent">React.js.</span>
         </p>
 
-        <p>
-          If you're looking for a "robot" developer, I'm not your guy, I like to
-          go deep into the business and understand it. If a requirement doesn't
-          feel right to me or I think there's a better way of solving a problem,
-          I'll speak up and do my best to sprout a productive discussion.
-        </p>
+        {/* <p>
+          If you're looking for a "robot" developer, I'm not your guy, I like to go deep
+          into the business and understand it. If a requirement doesn't feel right to me
+          or I think there's a better way of solving a problem, I'll speak up and do my
+          best to sprout a productive discussion.
+        </p> */}
       </div>
     </section>
   );
